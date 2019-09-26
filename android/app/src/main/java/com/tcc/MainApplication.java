262e9d84cc3f0007aa421bffbddc38777128edf9
Luiz Guilherme <30446223+luiz13lg@users.noreply.github.com>
2019-09-25 23:15:15 -0300

    Instalando e linkando realm

--- android/app/src/main/java/com/tcc/MainApplication.java
+++ android/app/src/main/java/com/tcc/MainApplication.java
@@ -3,6 +3,7 @@ package com.tcc;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import io.realm.react.RealmReactPackage;
 import com.sensors.RNSensorsPackage;
 import com.airbnb.android.react.maps.MapsPackage;
 import com.imagepicker.ImagePickerPackage;
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RealmReactPackage(),
             new RNSensorsPackage(),
             new MapsPackage(),
             new ImagePickerPackage()
